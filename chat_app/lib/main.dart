2f8ff096f3ca0277492b4a4a7daaba9c45333d72
android_shakil <shakil!@#$1234>
2021-06-09 11:12:12 +0600

    [Added] Firebase

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,58 @@
+// @dart=2.9
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp();
+  
+  runApp(ProviderScope(child: MyApp(app: app)));
+}
+
+class MyApp extends StatelessWidget {
+
+  FirebaseApp app;
+  MyApp({this.app});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page', app: app),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title, this.app}) : super(key: key);
+
+  final FirebaseApp app;
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(child: Text(widget.app.name),
+      ),
+    );
+  }
+}
